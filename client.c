6e8a086ca03727aaf4cf768207b37b3524645587
tufteddy <godisunstoppable@gmail.com>
2018-08-09 17:42:59 +0700

    back to local, add struct

--- client.c
+++ client.c
@@ -14,13 +14,30 @@ struct ClientToServer{
 	char nick[15];
 };
 
+struct gameClientToServer{
+	char move;
+};
+
+struct gameServerToClient{
+	short x_ball;
+	short y_ball;
+	short y_player1;
+	short y_player2;
+	char res;
+};
+
 int main(){
 	struct sockaddr_in serv_addr;
 	struct ServerToClient STC;
 	struct ClientToServer CTS;
+	struct gameClientToServer gCTS;
+	struct gameServerToClient gSTC;
 	int sockfd = 0, slen = sizeof(serv_addr), portnum = 15228;
 	
 	bzero((char *) &CTS, sizeof(CTS));
+	bzero((char *) &STC, sizeof(STC));
+	bzero((char *) &gCTS, sizeof(gCTS));
+	bzero((char *) &gSTC, sizeof(gSTC));
 	bzero((char *) &serv_addr, sizeof(serv_addr));
 	
 	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
@@ -30,7 +47,7 @@ int main(){
 	
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_port = htons(portnum);
-	inet_aton("109.174.125.180", &serv_addr.sin_addr);
+	inet_aton("127.0.0.1", &serv_addr.sin_addr);
 	
 	printf ("Input nickname: ");
 	fgets(CTS.nick, sizeof(CTS.nick), stdin);
