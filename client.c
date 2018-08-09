05c903d4ad79927c58dcd68cf3352fb3676cab9b
tufteddy <godisunstoppable@gmail.com>
2018-08-09 16:38:26 +0700

    Refactoring

--- client.c
+++ client.c
@@ -5,40 +5,41 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 
-struct structback{
+struct ServerToClient{
 	char nick[15];
 	short number;
 };
 
-struct structsend{
+struct ClientToServer{
 	char nick[15];
 };
 
 int main(){
 	struct sockaddr_in serv_addr;
-	struct structback cnctd;
-	struct structsend firstcon;
+	struct ServerToClient STC;
+	struct ClientToServer CTS;
+	int sockfd = 0, slen = sizeof(serv_addr), portnum = 15226;
 	
-	memset (cnctd.nick, 0, sizeof(cnctd.nick));
-	cnctd.number = 0;
-	
-	int sockfd, i, slen = sizeof(serv_addr), portnum = 8888;
-	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-	
+	bzero((char *) &CTS, sizeof(CTS));
 	bzero((char *) &serv_addr, sizeof(serv_addr));
+	
+	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+	if(sockfd <= 0){
+		perror ("socket");
+	}
+	
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_port = htons(portnum);
 	inet_aton("127.0.0.1", &serv_addr.sin_addr);
 	
 	printf ("Input nickname: ");
-	bzero((char *) &firstcon, sizeof(firstcon));
-	fgets(firstcon.nick, sizeof(firstcon.nick), stdin);
+	fgets(CTS.nick, sizeof(CTS.nick), stdin);
 	
-	sendto(sockfd, &firstcon, sizeof(firstcon), 0, (struct sockaddr *)&serv_addr, slen);
+	sendto(sockfd, &CTS, sizeof(CTS), 0, (struct sockaddr *)&serv_addr, slen);
 	
-	recvfrom(sockfd, &cnctd, sizeof(cnctd), 0, (struct sockaddr *)&serv_addr, &slen);
-	printf("Got back: %s", cnctd.nick);
-	printf("Number: %d\n", cnctd.number);
+	recvfrom(sockfd, &STC, sizeof(STC), 0, (struct sockaddr *)&serv_addr, &slen);
+	printf("Got back:\n%s", STC.nick);
+	printf("Number: %d\n", STC.number);
 
 	return 0;
 }
