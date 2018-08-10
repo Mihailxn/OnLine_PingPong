f1301b940b1554f0e33279b2eee74ef75763f292
root <root@Astral.(none)>
2018-08-10 14:41:17 +0700

    Add pthreads listener

--- serv.c
+++ serv.c
@@ -9,46 +9,81 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <pthread.h>
 #define X_FIELD 60//Размер поля x координаты
 #define Y_FIELD 30//Размер поля y координаты
 #define MID_RACKET 3//Размер середины ракетки (только нечётное число)
 #define SIDE_RACKET 2//Размер боковой части ракетки
 #define END_RACKET 1//Размер крайней части ракетки
 
-struct Vector//Структура движения шарика в следующий такт времени
+ struct Vector//Структура движения шарика в следующий такт времени
 {
     short x;
     short y;
 };
-
-struct ClientToServerConnect
+ struct ClientToServerConnect
 {
     char nikname[15];//Имя Игорька
 }/*CTSC*/;
-
-struct ServerToClientAccept
+ struct ServerToClientAccept
 {
     char nikname[15];//Имя противника
     short number;//Номер Игорька
 }/*STCA*/;
-
-struct ClientToServerGame
+ struct ClientToServerGame
 {
     char act;
 }/*CTSG*/;
-
-struct ServerToClientGame
+ struct ServerToClientGame
 {
-    short int x_boll;
-    short int y_boll;
+    short int x_ball;
+    short int y_ball;
     short int y_play_1;
     short int y_play_2;
     char status;
 }/*STCG*/;
 
-int main()
+
+struct ListenerArguments
 {
-    struct ClientToServerConnect CTSC;
+    short int *y_play;
+    int listener_1;
+    int client_address_size;
+    struct sockaddr_in *client;
+};
+
+//Функция слушателя игроков, принимает от них сообщения
+void *listener_fn(void *arguments)
+{
+    struct ListenerArguments *arg=(struct ListenerArguments *)arguments;
+    struct ClientToServerGame CTSG;
+    bzero((char* )&CTSG, sizeof(CTSG));
+    ssize_t status;
+    while(1)
+    {
+	//Без мьютексов так как изменяет данные только один данный поток, а чтением можно принебречь
+	if(status = recvfrom(arg.listener, &CTSG, sizeof(CTSG)+1, 0, (struct sockaddr *) arg->client,&arg.client_address_size) <0)
+	{
+		printf("recvfrom()");
+		exit(4);
+	} 
+	else 
+	{
+		if (CTSG.act = 'U')
+		{
+			arg->y_play += 3;
+		} 
+		if (CTSG.act = 'D')
+		{
+			arg->y_play -= 3;
+		}
+	}
+    }
+}
+
+ int main()
+{
+	struct ClientToServerConnect CTSC;
 	struct ServerToClientAccept STCA;
 	int listener_1, listener_2, client_address_size_1,client_address_size_2;
 	struct sockaddr_in addr, client_1,client_2;//endpoint сервера, первого клиента, второго клиента
@@ -75,8 +110,7 @@ int main()
     	    printf("recvfrom()");
     	    exit(4);
 	}
-	STCA.number=1;
-	strncpy(STCA.nikname,CTSC.nikname,sizeof(STCA.nikname));
+	
     
 	client_address_size_2 = sizeof(client_2);
 	if(recvfrom(listener_1, &CTSC, sizeof(CTSC)+1, 0, (struct sockaddr *) &client_2,&client_address_size_2) <0)
@@ -92,6 +126,8 @@ int main()
 		exit(1); /*выход из родительского процесса*/
 	    case 0:
 	    {
+			STCA.number=1;
+			strncpy(STCA.nikname,CTSC.nikname,sizeof(STCA.nikname));
 		if (sendto(listener_1, &STCA, sizeof(STCA), 0,(struct sockaddr *)&client_1, sizeof(client_1)) < 0)
 		{
 		    printf("sendto()");
@@ -111,41 +147,83 @@ int main()
 		struct Vector vct;
 		short y_play_1=Y_FIELD/2;
 		short y_play_2=Y_FIELD/2;
-		struct ClientToServerGame CTSG=malloc(sizeof(struct ClientToServerGame));
-		struct ServerToClientGame STCG=malloc(sizeof(struct ServerToClientGame));;
+		struct ClientToServerGame CTSG;
+		bzero((char* )&CTSG, sizeof(CTSG));
+		struct ServerToClientGame STCG;
+		bzero((char* )&STCG, sizeof(STCG));
+		//ssize_t status;
 		
 		while(vct.x=rand()%3-1==0);
-		vct.y=rand()%3-1;
+			vct.y=rand()%3-1;
+			
+			
+		struct ListenerArguments LA1;//Аргумент для слушателя игрока слева
+		LA1.listener=listener_1;
+		LA1.client_address_size=client_address_size_1;
+		LA1->client=&client_1;
+		LA1->y_play=&y_play_1;
+		
+		struct ListenerArguments LA2;//Аргумент для слушателя игрока справа
+		LA2.listener=listener_1;
+		LA2.client_address_size=client_address_size_2;
+		LA2->client=&client_2;
+		LA2->y_play=&y_play_2;
+		
+		pthread_t p_listener_1;
+		pthread_t p_listener_2;
+		pthread_create(&p_listener_1,NULL,listener_fn,(void*)LA1);
+		pthread_create(&p_listener_2,NULL,listener_fn,(void*)LA2);
+		
 		while(1)
 		{
-		    //____________________________________Место для приёма данных select()_____________________________________
-		    
-		    
-		    //__________________________________________________Даня действуй__________________________________________
-		    STCG.x_ball=x_ball;
-		    STCG.y_ball=y_ball;
-		    STCG.x_play_1=x_play_1;
-		    STCG.x_play_2=x_play_2;
-		    STCG.status="G";
-		    //Отправляем сообщение игроку слева
+			STCG.status='G';
+			STCG.x_ball=x_ball;
+			STCG.y_ball=y_ball;
+			STCG.y_play_1=y_play_1;
+			STCG.y_play_2=y_play_2;
+			
+			/*if(status = recvfrom(listener_1, &CTSG, sizeof(CTSG)+1, MSG_DONTWAIT, (struct sockaddr *) &client_1,&client_address_size_1) <0)
+			{
+				printf("recvfrom()");
+				exit(4);
+			} else {
+				if (CTSG.act = 'U'){
+					y_play_1 += 3;
+				} else {
+					y_play_1 -= 3;
+				}
+			}
+			if(status = recvfrom(listener_1, &CTSG, sizeof(CTSG)+1, MSG_DONTWAIT, (struct sockaddr *) &client_2,&client_address_size_2) <0)
+			{
+				printf("recvfrom()");
+				exit(4);
+			} else {
+				if (CTSG.act = 'U'){
+					y_play_2 += 3;
+				} else {
+					y_play_2 -= 3;
+				}
+			}*/
+			
+ 		    //Отправляем сообщение игроку слева
 		    if(x_ball>=X_FIELD)
-			STCG.status="V";
+				STCG.status='V';
 		    if(x_ball<=0)
-			STCG.status="L";
+				STCG.status='L';
 		    if (sendto(listener_1, &STCG, sizeof(STCG), 0,(struct sockaddr *)&client_1, sizeof(client_1)) < 0)
 		    {
-			printf("sendto()");
-			exit(2);
+				printf("sendto()");
+				exit(2);
 		    }
 		    //Отправляем сообщение игроку справа
 		    if(x_ball>=X_FIELD)
-		        STCG.status="L";
+		        STCG.status='L';
 		    if(x_ball<=0)
-			STCG.status="V";
+				STCG.status='V';
 		    if (sendto(listener_1, &STCG, sizeof(STCG), 0,(struct sockaddr *)&client_2, sizeof(client_2)) < 0)
 		    {
-			printf("sendto()");
-			exit(2);
+				printf("sendto()");
+				exit(2);
 		    }
 		    
 		    //вынести в функцию но мне пока лень__________________________
@@ -155,7 +233,7 @@ int main()
 			    x_ball=X_FIELD/2;
 			    y_ball=Y_FIELD/2;
 			    while(vct.x=rand()%3-1==0);
-			    vct.y=rand()%3-1;
+					vct.y=rand()%3-1;
 		    }
 		    //Если мяч улетел за левого игрока
 		    if(x_ball<=0)
@@ -163,80 +241,84 @@ int main()
 			    x_ball=X_FIELD/2;
 			    y_ball=Y_FIELD/2;
 			    while(vct.x=rand()%3-1==0);
-			    vct.y=rand()%3-1;
+					vct.y=rand()%3-1;
 		    }
 		    
 		    //Мяч ударился о потолок
 		    if(y_ball==1)
 		    {
-			vct.y=-vct.y;
+				vct.y=-vct.y;
 		    }
 		    //Мяч ударился о пол
 		    if(y_ball==Y_FIELD-1)
 		    {
-			vct.y=-vct.y;
+				vct.y=-vct.y;
 		    }
 		    
 		    //Мяч ударился о центр ракетки игрока справа
 		    if(x_ball==X_FIELD-1&&(y_ball>=(y_play_2-MID_RACKET/2)&&y_ball<=(y_play_2+MID_RACKET/2)))
 		    {
-			vct.x=-1;
+				vct.x=-1;
 		    }
 		    //Мяч ударился о верхний бок ракетки игрока справа
 		    if(x_ball==X_FIELD-1&&(y_ball>(y_play_2+MID_RACKET/2)&&y_ball<=(y_play_2+MID_RACKET/2+SIDE_RACKET)))
 		    {
-			vct.x=-1;
-			vct.y--;
+				vct.x=-1;
+				vct.y--;
 		    }
 		    //Мяч ударился о нижний бок ракетки игрока справа
 		    if(x_ball==X_FIELD-1&&(y_ball<(y_play_2-MID_RACKET/2)&&y_ball>=(y_play_2-MID_RACKET/2-SIDE_RACKET)))
 		    {
-			vct.x=-1;
-			vct.y++;
+				vct.x=-1;
+				vct.y++;
 		    }
 		    //Мяч ударился о верхний край ракетки игрока справа
-		    if(x_ball==X_FIELD-1&&(y_ball>(y_play_2+MID_RACKET/2+SIDE_RACEK)&&y_ball<=(y_play_2+MID_RACKET/2+SIDE_RACKET+END_RACKET)))
+		    if(x_ball==X_FIELD-1&&(y_ball>(y_play_2+MID_RACKET/2+SIDE_RACKET)&&y_ball<=(y_play_2+MID_RACKET/2+SIDE_RACKET+END_RACKET)))
 		    {
-			vct.x=-1;
-			vct.y=vct.y-2;
+				vct.x=-1;
+				vct.y=vct.y-2;
 		    }
 		    //Мяч ударился о нижний край ракетки игрока справа
 		    if(x_ball==X_FIELD-1&&(y_ball<(y_play_2-MID_RACKET/2-END_RACKET)&&y_ball>=(y_play_2-MID_RACKET/2-SIDE_RACKET-END_RACKET)))
 		    {
-			vct.x=-1;
-			vct.y=vct.y+2;
+				vct.x=-1;
+				vct.y=vct.y+2;
 		    }
 		    
 		    //Мяч ударился о центр ракетки игрока слева
 		    if(x_ball==1&&(y_ball>=(y_play_1-MID_RACKET/2)&&y_ball<=(y_play_1+MID_RACKET/2)))
 		    {
-			vct.x=1;
+				vct.x=1;
 		    }
 		    //Мяч ударился о верхний бок ракетки игрока слева
 		    if(x_ball==1&&(y_ball>(y_play_1+MID_RACKET/2)&&y_ball<=(y_play_1+MID_RACKET/2+SIDE_RACKET)))
 		    {
-			vct.x=-1;
-			vct.y--;
+				vct.x=-1;
+				vct.y--;
 		    }
 		    //Мяч ударился о нижний бок ракетки игрока слева
 		    if(x_ball==1&&(y_ball<(y_play_1-MID_RACKET/2)&&y_ball>=(y_play_1-MID_RACKET/2-SIDE_RACKET)))
 		    {
-			vct.x=-1;
-			vct.y++;
+				vct.x=-1;
+				vct.y++;
 		    }
 		    //Мяч ударился о верхний край ракетки игрока слева
-		    if(x_ball==1&&(y_ball>(y_play_1+MID_RACKET/2+SIDE_RACEK)&&y_ball<=(y_play_1+MID_RACKET/2+SIDE_RACKET+END_RACKET)))
+		    if(x_ball==1&&(y_ball>(y_play_1+MID_RACKET/2+SIDE_RACKET)&&y_ball<=(y_play_1+MID_RACKET/2+SIDE_RACKET+END_RACKET)))
 		    {
-			vct.x=-1;
-			vct.y=vct.y-2;
+				vct.x=-1;
+				vct.y=vct.y-2;
 		    }
 		    //Мяч ударился о нижний край ракетки игрока слева
 		    if(x_ball==1&&(y_ball<(y_play_1-MID_RACKET/2-END_RACKET)&&y_ball>=(y_play_1-MID_RACKET/2-SIDE_RACKET-END_RACKET)))
 		    {
-			vct.x=-1;
-			vct.y=vct.y+2;
+				vct.x=-1;
+				vct.y=vct.y+2;
 		    }
-		    //___________________________________________
+		    //Меняем позицию мячика
+		    x_ball+=vct.x;
+		    y_ball+=vct.y;
+		    
+		    sleep(5);//Пока подольше для отладки
 		}
 		exit(0);
 	    }
